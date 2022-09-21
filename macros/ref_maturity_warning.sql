a0b2648f0435dde4871605cda8e833e87d08fdf7
Nicholas Yager <nicholas.yager@dbtlabs.com>
2022-09-21 10:40:23 -0400

    fix: Added maturity reference warning to materialization storage code

--- macros/ref_maturity_warning.sql
+++ macros/ref_maturity_warning.sql
@@ -1,49 +1,33 @@
-{% macro ref_maturity_warning(warning_maturity_levels) %}
+{% macro ref_maturity_warning(current_model, warning_maturity_levels) %}
 
-    {# We can only access the model and graph objects during execution. #}
-    {% if execute %}
-
-        {% do log('===== Checking for Immature Model References ======', info=True) %}
-
-        {# Introspect to get the current model's dependencies. #}
-        {% for current_model in get_models_from_graph() %}
-
-            {# Create a model list to store any low-maturity models #}
-            {% set immature_model_list = [] %}
-            {% set dependencies = get_model_dependency_nodes(current_model) %}
-
-            {# Now, grab the graph node for this reference#}
-            {% for node in dependencies %}
-
-                {# Check the maturity! If it's "low", then add it to the model_list #}
-                {% set maturity = node.config.meta.maturity %}
-                {% if maturity in warning_maturity_levels %}
-                    {% do immature_model_list.append(node.name) %}
-                {% endif %}
-
-            {% endfor %}
-
-            {# If there are items in model_list, then raise a warning. #}
-            {% if immature_model_list | length > 0 %}
-                {% do exceptions.warn(
-                    "WARNING: The " ~ current_model.name ~ " model references " ~  immature_model_list | length
-                    ~ " models with a low maturity. Models: " ~ ', '.join(immature_model_list)
-                ) %}
-            {% endif %}
-
-        {% endfor %}
-
-        {% do log('===== Immature Model Reference Check Complete =====', info=True) %}
-
+    {# We can only access graph object during execution. #}
+    {% if not execute %}
+        {{ return() }}
     {% endif %}
 
-{% endmacro %}
 
+    {# Create a model list to store any low-maturity models #}
+    {% set immature_model_list = [] %}
 
-{% macro get_models_from_graph() %}
-    {% set models = graph.nodes.values()
-        | selectattr("resource_type", "equalto", 'model')%}
-    {{ return(models) }}
+    {# Now, grab the graph node for this reference#}
+    {% for node in get_model_dependency_nodes(current_model) %}
+
+        {# Check the maturity! If it's "low", then add it to the model_list #}
+        {% if node.config.meta.maturity in warning_maturity_levels %}
+            {% do immature_model_list.append(node.name) %}
+        {% endif %}
+
+    {% endfor %}
+
+    {# If there are items in model_list, then raise a warning. #}
+    {% if immature_model_list | length > 0 %}
+        {{ log(
+            "WARNING: The " ~ current_model.name ~ " model references immature models. " ~
+            "Models: " ~ ', '.join(immature_model_list),
+            info=True
+        ) }}
+    {% endif %}
+
 {% endmacro %}
 
 
@@ -51,24 +35,17 @@
 
     {% set dependencies = [] %}
 
-    {%- set references = current_model.depends_on.nodes -%}
-
-    {% for reference_unique_id in references %}
-
-        {# If we're not referencing a model, then continue. #}
-        {% if not reference_unique_id.startswith('model') %}
-            {% do continue %}
-        {% endif %}
+    {% for reference_unique_id in current_model.depends_on.nodes %}
 
         {# Now, grab the graph node for this reference#}
         {% for node in graph.nodes.values()
             | selectattr("unique_id", "equalto", reference_unique_id) %}
-
             {% do dependencies.append(node) %}
 
         {% endfor %}
 
     {% endfor %}
+
     {{ return(dependencies) }}
 
 {% endmacro %}
