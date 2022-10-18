e777cb73a7c211f929fc96b69311ceff242dc195
wasilaq <91074396+wasilaq@users.noreply.github.com>
2022-10-18 09:19:32 -0500

    Update branch name
    Co-authored-by: Nicholas A. Yager <nicholas.yager@dbtlabs.com>

--- models/reporting/rpt_department_warehouse_usage.sql
+++ models/reporting/rpt_department_warehouse_usage.sql
@@ -12,7 +12,7 @@
 {#
 
 In order to successfully run this model, first ensure you have a working store_materialization_results macro and then do a dbt run.
-You can copy and paste a working solution from the checkpoint_1 branch if you need to. 
+You can copy and paste a working solution from the _checkpoint_1 branch if you need to. 
 
 Secondly, you will need to go into the following model files and enable them by deleting the config block and saving:
 
